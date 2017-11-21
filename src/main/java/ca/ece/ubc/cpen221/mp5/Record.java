14cebc23372466de967d923dfb2de28172051cfa
Nancy <nancyjiang10@gmail.com>
2017-11-21 13:27:27 -0800

    bloop

--- src/main/java/ca/ece/ubc/cpen221/mp5/Record.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Record.java
@@ -1,8 +1,84 @@
 package ca.ece.ubc.cpen221.mp5;
 
-public class Record {
+/**
+ * Most generic data type for an entry in the database
+ * 
+ * @author nancy
+ *
+ */
+
+public abstract class Record {
 	protected String id;
 	protected String type;
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getId() {
+		return id;
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getType() {
+		return type;
+	}
+	
+	/**
+	 * 
+	 * @param type
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Record other = (Record) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
 	
 	
 }
