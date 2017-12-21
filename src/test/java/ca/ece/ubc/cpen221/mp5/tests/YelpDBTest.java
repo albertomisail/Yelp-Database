102bfb1c50187059d887394f03421b7b73cf4762
Nancy <nancyjiang10@gmail.com>
2017-12-20 18:40:20 -0800

    tests..

--- src/test/java/ca/ece/ubc/cpen221/mp5/tests/YelpDBTest.java
+++ src/test/java/ca/ece/ubc/cpen221/mp5/tests/YelpDBTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertEquals;
 public class YelpDBTest {
     @Test
     public void test1() throws IOException {
-        YelpDB database = new YelpDB("data/restaurants.json","data/reviews.json","data/users.json");
+        YelpDB database = new YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json");
         assertEquals(true, database.containsUser("754HGCLgGJLh1VU_WtGjsw"));
         System.out.println(database.getUser("754HGCLgGJLh1VU_WtGjsw"));
         assertEquals(true, database.containsProduct("BJKIoQa5N2T_oDlLVf467Q"));
@@ -19,7 +19,7 @@ public class YelpDBTest {
         assertEquals(true, database.containsReview("0f8QNSVSocn40zr1tSSGRw"));
         System.out.println(database.getReview("0f8QNSVSocn40zr1tSSGRw"));
         assertEquals(true, database.getUser(database.getReview("0f8QNSVSocn40zr1tSSGRw").getUser_id()).getReviews()
-                                        .contains("0f8QNSVSocn40zr1tSSGRw"));
+                .contains("0f8QNSVSocn40zr1tSSGRw"));
         assertEquals(false, database.containsProduct("lalal"));
         assertEquals(16, database.getMatches("category(Chinese)||in(Hearst Ave)").size());
         System.out.println(database.getMatches("in(Telegraph Ave) && category(Chinese) || category(Italian) && price <= 2 "));
@@ -30,14 +30,15 @@ public class YelpDBTest {
     }
 
     @Test
-    public void test2() throws IOException{
-        YelpDB database = new YelpDB("data/restaurants.json","data/reviews.json","data/users.json");
+    //adding restaurants + users
+    public void test2() throws IOException {
+        YelpDB database = new YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json");
         Set<YelpRestaurant> s = database.getMatches("in(Telegraph Ave) && category(Chinese) || category(Italian) && price <= 2 ");
         System.out.println(s.size());
-        for(YelpRestaurant r : s){
+        for (YelpRestaurant r : s) {
             System.out.println(r);
         }
-       Set<YelpRestaurant> s2 =  database.getMatches("name(bloop)");
+        Set<YelpRestaurant> s2 = database.getMatches("name(bloop)");
         assertEquals(0, s2.size());
         YelpRestaurant bloop = new YelpRestaurant("{\"open\": true, \"url\": \"http://www.yelp.com/biz/the-toaster-oven-berkeley\", \"longitude\": -122.2590117, \"neighborhoods\": [\"Telegraph Ave\", \"UC Campus Area\"], \"name\": \"bloop\", \"categories\": [\"Sandwiches\", \"Restaurants\"], \"state\": \"CA\", \"type\": \"business\", \"stars\": 3.5, \"city\": \"Berkeley\", \"full_address\": \"2309 Telegraph Ave\\nTelegraph Ave\\nBerkeley, CA 94704\", \"review_count\": 55, \"photo_url\": \"http://s3-media2.ak.yelpcdn.com/bphoto/1IirdUSB0yi4TorBV5JPoA/ms.jpg\", \"schools\": [\"University of California at Berkeley\"], \"latitude\": 37.8684118, \"price\": 1}", database);
         database.addProduct(bloop);
@@ -53,5 +54,17 @@ public class YelpDBTest {
         assertEquals(0, userx.getReviews().size());
         userx.addReview("12345");
         assertEquals(1, userx.getReviews().size());
-        }
+    }
+
+    //adding reviews
+    @Test
+    public void test3() throws IOException {
+        YelpDB database = new YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json");
+        YelpReview review = new YelpReview("{\"type\": \"review\", \"business_id\": \"1CBs84C-a-cuA3vncXVSAw\", \"votes\": {\"cool\": 0, \"useful\": 0, \"funny\": 0}, \"text\": \"Great pizza and salads!\", \"stars\": 4, \"user_id\": \"Tq13lhXLgpOQHmp9CxoIkA\", \"date\": \"2012-05-25\"}", database);
+        assertEquals(false, database.containsReview(review.getId()));
+        database.addReview(review);
+        assertEquals(true, database.containsReview(review.getId()));
+    }
 }
+
+
